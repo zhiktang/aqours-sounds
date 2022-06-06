ace3b7dfd4e9f20f5c4410aa89a39fd664c1f6f7
zhiktang <zhikai.tang123@gmail.com>
2022-06-06 04:21:35 -0500

    send no js to test

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.get("/", function(req, res) {
     res.sendFile(__dirname + "/index.html");
     res.sendFile(__dirname + "/style.css");
-    req.sendFile(__dirname + "/script.js");
+    
   });
 app.post('/', function(req, res) {
    // console.log(req.body);
