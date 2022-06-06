647da96022d1be1614cc70abf0ceef862ddca9df
zhiktang <zhikai.tang123@gmail.com>
2022-06-06 04:22:07 -0500

    typo

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ var app = express();
 app.use(bodyParser.urlencoded({ extended: true }));
 app.get("/", function(req, res) {
     res.sendFile(__dirname + "/index.html");
-    res.sendFile(__dirname + "/style.css");
+    res.sendFile(__dirname + "/styles.css");
     
   });
 app.post('/', function(req, res) {
