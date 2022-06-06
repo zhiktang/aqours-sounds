b35821f0ed2bc0c47bbcd7bd673110800fcbb153
zhiktang <zhikai.tang123@gmail.com>
2022-06-06 03:55:25 -0500

    getting a server response is overrated

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 var bodyParser = require('body-parser');
 var express = require('express');
+const fs = require('fs');
 var app = express();
 app.use(bodyParser.urlencoded({ extended: true }));
 app.post('/server.js', function(req, res) {
@@ -8,4 +9,10 @@ app.post('/server.js', function(req, res) {
    res.send('Got a POST request');
    // res.send(req.body);
    // res.send(count.txt)
+   fs.writeFile('test.txt', req.body.person, function(err) {
+       if(err) {
+           return console.log(err);
+       }
+       console.log("The file was saved!");
+   });
     });
