476da343def32bf41b8695b6d3bb2225677d91d9
zhiktang <zhikai.tang123@gmail.com>
2022-06-07 17:06:59 -0500

    we do a lil json.stringify

--- script.js
+++ script.js
@@ -53,7 +53,7 @@ function click(person) {
     const xhttp = new XMLHttpRequest();
     xhttp.open('POST', 'http://192.168.1.137:3000', true);
     xhttp.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
-    xhttp.send(person);
+    xhttp.send(JSON.stringify({person: person}));
     xhttp.onreadystatechange = function() {
         if(this.readyState == 4 && this.status == 200) {
             console.log(this.responseText);
--- server.js
+++ server.js
@@ -4,41 +4,11 @@ const fs = require('fs');
 var app = express();
 app.use(bodyParser.urlencoded({ extended: true }));
 app.post('/', function(req, res) {
-   //console.log(req.body);
+   console.log(req.body);
    //console.log(req.body.bodyParser);
    //console.log(req.body.sound);
    //console.log(req.body.count);
-   if (req.body = '{ Riko: \'\'}'){
-        console.log("Riko");
-   }
-    else if (req.body = '{ Chika: \'\'}'){
-        console.log("Chika");
-    }
-    else if (req.body = '{ You: \'\'}'){
-        console.log("You");
-    }
-    else if (req.body = '{ Dia: \'\'}'){
-        console.log("Dia");
-    }
-    else if (req.body = '{ Kanan: \'\'}'){
-        console.log("Kanan");
-    }
-    else if (req.body = '{ Mari: \'\'}'){
-        console.log("Mari");
-    }
-    else if (req.body = '{ Hanamaru: \'\'}'){
-        console.log("Hanamaru");
-    }
-    else if (req.body = '{ Ruby: \'\'}'){
-        console.log("Ruby");
-    }
-    else if (req.body = '{ Yoshiko: \'\'}'){
-        console.log("Yoshiko");
-    }
-    else {
-        console.log("error");
-    }
-    
+    console.log(req.body.person);
     //console.log('post');
    res.send('Got a POST request');
    // res.send(req.body);
