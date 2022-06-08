8caf6e4e85850f1580c44a779d7e35a8d55863c0
zhiktang <zhikai.tang123@gmail.com>
2022-06-08 13:15:21 -0500

    update on load

--- script.js
+++ script.js
@@ -5,6 +5,7 @@ sounds.forEach((sound)=> {
     const btn = document.createElement('button');
     btn.classList.add('btn');
     btn.innerText = sound;
+    update();
     btn.addEventListener('click', ()=> {
         //console.log(sound);
         click(sound);
