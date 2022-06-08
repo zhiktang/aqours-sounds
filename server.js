b6ed6ebe6afc57e1130b5430067f30142aa3f22c
zhiktang <zhikai.tang123@gmail.com>
2022-06-08 15:35:51 -0500

    put write inside the read

--- server.js
+++ server.js
@@ -40,14 +40,15 @@ app.post('/', function(req, res) {
             }
             output += lines[i] + '\n';
         }  
-    });
-    console.log("output is: " + output);
+        console.log("output is: " + output);
     fs.writeFile('count.txt', output, function(err) {
         if(err) {
             return console.log(err);
         }
         console.log("The file was saved!");
     });
+    });
+    
 });
     app.listen(3000, function(){
         console.log("server is running on port 3000");
