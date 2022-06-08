db737695b2571d5c33641364cf04d6728ffac805
zhiktang <zhikai.tang123@gmail.com>
2022-06-08 15:40:48 -0500

    removed verbose

--- server.js
+++ server.js
@@ -5,14 +5,14 @@ var app = express();
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.post('/', function(req, res) {
-   console.log(req.body);
+   //console.log(req.body);
    //console.log(req.body.bodyParser);
    //console.log(req.body.sound);
    //console.log(req.body.count);
     console.log(req.body.person);
     var person = req.body.person;
     //console.log('post');
-   res.send('Got a POST request');
+   //res.send('Got a POST request');
    // res.send(req.body);
    // res.send(count.txt)
    /*fs.writeFile('count.txt', req.body.count, function(err) {
@@ -24,32 +24,32 @@ app.post('/', function(req, res) {
     */
    var output = '';
     fs.readFile('count.txt', 'utf-8' , (err, data) => {
-        console.log("reading file");
+        //console.log("reading file");
         if(err) {
             return console.log(err);
         }
-        console.log("data is" + data);
+        //console.log("data is" + data);
         var lines = data.split('\n');
         for (var i = 0; i < lines.length; i++) {
-            console.log("Line " + i + " is: " + lines[i]);
+            //console.log("Line " + i + " is: " + lines[i]);
             if (lines[i].startsWith(person)) {
                 var data = lines[i].split(' ');
                 data[1] = parseInt(data[1]) + 1;
                 lines[i] = data.join(' ');
-                console.log("modified line is: " + lines[i]);
+                //console.log("modified line is: " + lines[i]);
             }
             output += lines[i];
             if(i < lines.length - 1) {
                 output += '\n';
             }
-            
+
         }  
-        console.log("output is: " + output);
+       // console.log("output is: " + output);
     fs.writeFile('count.txt', output, function(err) {
         if(err) {
             return console.log(err);
         }
-        console.log("The file was saved!");
+        //console.log("The file was saved!");
     });
     });
     
