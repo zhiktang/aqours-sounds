bcafac03de933e9a58b3fb7e395da0ed130dfb36
zhiktang <zhikai.tang123@gmail.com>
2022-06-13 20:12:54 -0500

    revert back to dev

--- script.js
+++ script.js
@@ -55,7 +55,7 @@ function update() {
 function click(person) {
     //console.log(person);
     const xhttp = new XMLHttpRequest();
-    xhttp.open('POST', 'https://count.alhub.net', true);
+    xhttp.open('POST', 'http://192.168.1.137:3000', true);
     xhttp.setRequestHeader('Content-type', 'application/json');
     xhttp.send(JSON.stringify({person: person}));
     xhttp.onreadystatechange = function() {
